00804aace50a6d7e09b175bb33da0c7c43ed8c56
Vanessa <160286052+AmaCodez@users.noreply.github.com>
2024-05-23 14:25:44 -0400

    'Add new function' 'Delete pseudocode'

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-// Create a function named getComputerChoice
-// Create a variable named num that gives
-// a random number from 0 to 3
-// if the number is equal to 0 print 'rock'
-// if the number is equal to 1 print 'paper'
-// otherwise print 'scissors'
-// Test and check the result
 
 
 function getComputerChoice(){
@@ -17,80 +10,81 @@ function getComputerChoice(){
         return 'scissors'
     }
 }
-console.log(getComputerChoice());
-
-// Create a function named getHumanChoice 
-//Create a variable named humanChoice 
-//that takes in the users message
-//and returns it
-// if humanChoice equals to 'rock' print 'rock'
-// if humanChoice equals to 'paper' print 'paper'
-// if humanChoice equals to 'scissors' print 'scissors'
-//otherwise print'try again'
+// console.log(getComputerChoice());
 
 
 function getHumanChoice(){
-    let humanUser = prompt("What's your choice?");
-    if (humanUser == 'rock'){
-        return 'rock';
-    } else if (humanUser == 'paper'){
-        return 'paper';
-    } else if (humanUser == 'scissors'){
-        return 'scissors'
+    let humanUser = prompt("What's your choice? (rock, paper, or scissors)").toLowerCase();
+    if (humanUser === 'rock' || humanUser === 'paper' || humanUser === 'scissors'){
+        return humanUser;
     } else {
-        return 'Mmh.. wrong input'
+        alert ('Invalid input. Please enter rock, paper, or scissors');
     }
 }
 
-console.log(getHumanChoice());
+// console.log(getHumanChoice());
 
-// Store player scores in a variable (store answers)
-// Create a variable named humanScore with a value of 0
-//Create a variable named computerScore with a value of 0
+let humanScore = 0;
+let computerScore = 0;
+   
+    function playRound (humanChoice, computerChoice){
+        console.log(`Human: ${humanChoice}, Computer: ${computerChoice}`);
+        if (humanChoice == computerChoice){
+         return "It's a tie!";
+        } else if (humanChoice == 'rock' && computerChoice == 'scissors'){
+        humanScore++;
+        return 'You win! Rock beats Scissors';
+        } else if (humanChoice == 'paper' && computerChoice == 'rock'){
+        humanScore++;
+        return 'You win! Paper beats Rock';
+        } else if (humanChoice == 'scissors' && computerChoice == 'paper'){
+        humanScore++;
+        return 'You win! Scissors beats Paper';
+        } else if (computerChoice == 'rock' && humanChoice == 'scissors'){
+        computerScore++;
+         return 'Computer wins! Rock beats Scissors';
+        } else if (computerChoice == 'paper' && humanChoice == 'rock'){
+        computerScore++;
+         return 'Computer wins! Paper beats Rock';
+        } else if (computerChoice == 'scissors' && humanChoice == 'paper'){
+        computerScore++;
+        return 'Computer wins! Scissors beats Paper';  
+        } else {
+         return 'Play again'
+        }
+     }
 
-const humanScore = 0;
-const computerScore = 0;
-
-//Game would be played as a round. A new function will take 
-// both the user and computers choice as arguments
-// it will play a single round and after that round the winner's score is 
-// incremented and a winner annoncement will be printed
-// Create a function named playRound and include two parameters for human and computer
-//Make the human parameter case-insensitive
-//Create a new variable named humanSelection to store the function getComputerChoice
-//Create a new variable named computerSelection to store the function getHumanChoice
-//if humanchoice == computer choice print 'tie'
-//if humanchoice == 'rock' and computerchoice == 'paper'
-//Increase the score of computer by 1 and print 'Computer wins'
-// repeat method
-
-function playRound (human, computer){
-   if (humanChoice == computerChoice){
-    return "It's a tie! Play again";
-   } else if (humanChoice == 'rock' && computerChoice == 'scissors'){
-    return 'You win! Rock beats Paper';
-    humanScore++;
-   } else if (humanChoice == 'paper' && computerChoice == 'rock'){
-    return 'You win! Paper beats Rock';
-    humanScore++;
-   } else if (humanChoice == 'scissors' && computerChoice == 'paper'){
-    return 'You win! Scissors beats Paper';
-    humanScore++;
-   } else if (computerChoice == 'rock' && humanChoice == 'scissors'){
-    return 'Computer wins! Rock beats Paper';
-    computerScore++;
-   } else if (computerChoice == 'paper' && humanChoice == 'rock'){
-    return 'Computer wins! Paper beats Rock';
-    computerScore++;
-   } else if (computerChoice == 'scissors' && humanChoice == 'paper'){
-    return 'Computer wins! Scissors beats Paper';
-    computerScore++;
-   } else {
-    return 'Play again'
-   }
+function gameScore(humanResult, computerResult){
+    // console.log(`Human Score: ${humanResult}, Computer score: ${computerResult}`)
+    if (humanResult == computerResult){
+        return "It's a tie";
+    } else if (humanResult > computerResult){
+        return 'You win!';
+    } else {
+        return 'Computer wins!';
+    }
 }
 
-const humanChoice = getHumanChoice();
-const computerChoice = getComputerChoice();
-console.log(playRound(humanChoice, computerChoice));
+// Use for loop to loop the playRound function
+// initial value should be 1 and the condition should be looped to 5
+// increment it by 1
+//Loop the playRound funciton
+// Test your code on console
+function playGame(){
+let gameRound;
+for (gameRound = 1; gameRound <= 5; gameRound++){
+    const humanSelection = getHumanChoice();
+     const computerSelection = getComputerChoice();
 
+    console.log(`Round ${gameRound}`);
+    console.log(playRound(humanSelection, computerSelection));
+    console.log(`Human Score: ${humanScore}, Computer Score: ${computerScore}` );
+
+}
+
+console.log('----GAME OVER----')
+console.log(gameScore(humanScore, computerScore));
+
+}
+
+console.log(playGame());
